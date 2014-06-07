9bc6426bdd0237da2949577bfd3d24eb405fd6e1
KITAITI Makoto <KitaitiMakoto@gmail.com>
2014-06-08 05:28:49 +0900

    Implement generic URN scheme

--- lib/uri/urn.rb
+++ lib/uri/urn.rb
@@ -1,5 +1,90 @@
+require 'uri/generic'
+
 module URI
-  class URN
-    # Your code goes here...
+  class URN < Generic
+    COMPONENT = [:scheme, :nid, :nss]
+
+    NID_PATTERN = '[\w\d][\w\d\-]{0,31}'.freeze
+    URN_CHARS_PATTERN = "[\\w\\d()+,\\-.:=@;$_!*%/?#]|#{PATTERN::ESCAPED}".freeze
+    NSS_PATTERN = "(?:#{URN_CHARS_PATTERN})+".freeze
+    URN_REGEXP = Regexp.new("\\A(?<nid>#{NID_PATTERN}):(?<nss>#{NSS_PATTERN})\\z").freeze
+
+    attr_reader :nid, :nss
+
+    @@nids = {}
+    def self.nid_list
+      @@nids
+    end
+
+    def self.build(args)
+      tmp = Util.make_components_hash(self, args)
+      tmp[:opaque] = "%{nid}:%{nss}" % tmp
+
+      return super(tmp)
+    end
+
+    def initialize(*arg)
+      super(*arg)
+      self.opaque = @opaque
+    end
+
+    def nid=(value)
+      check_nid(value)
+      set_nid(value)
+      value
+    end
+
+    def nss=(value)
+      check_nss(value)
+      set_nss(value)
+      value
+    end
+
+    def opaque=(value)
+      check_opaque(value)
+
+      if URN_REGEXP =~ value
+        self.nid = $~['nid']
+        self.nss = $~['nss']
+      else
+        raise InvalidURIError, "bad URI(nid nor nss not set?): #{self}"
+      end
+
+      set_opaque(value)
+      value
+    end
+
+    def normalize!
+      super
+      set_nid(self.nid.downcase)
+    end
+
+    protected
+
+    def set_nid(nid)
+      @nid = nid
+    end
+
+    def set_nss(nss)
+      @nss = nss
+    end
+
+    private
+
+    def check_nid(value)
+      if value !~ /\A#{NID_PATTERN}\z/o
+        raise InvalidComponentError, "bad component(expected nid component: #{value})"
+      end
+
+      return true
+    end
+
+    def check_nss(value)
+      if value !~ /\A#{NSS_PATTERN}\z/o
+        raise InvalidComponentError, "bad component(expected nss component: #{value})"
+      end
+    end
   end
+
+  @@schemes['URN'] = URN
 end
--- test/uri/test_urn.rb
+++ test/uri/test_urn.rb
@@ -4,8 +4,49 @@ require 'uri/urn'
 module URI
 
 class TestURN < Test::Unit::TestCase
-  def test_a
-    assert false
+  def setup
+    @uri_string = 'urn:nid:nss'
+    @uri = URI.parse(@uri_string)
+  end
+
+  def test_build
+    urn = nil
+    assert_nothing_raised do
+      urn = URI::URN.build(['nid', 'nss'])
+    end
+    assert_equal 'nid', urn.nid
+    assert_equal 'nss', urn.nss
+    assert_equal 'nid:nss', urn.opaque
+  end
+
+  def test_parse
+    assert_nothing_raised do
+      URI.parse @uri_string
+    end
+    [
+      'urn:',
+      'urn:sp ce'
+    ].each do |uri_string|
+      assert_raise InvalidURIError do
+        URI.parse uri_string
+      end
+    end
+  end
+
+  def test_component
+    assert_equal [:scheme, :nid, :nss], @uri.component
+  end
+
+  def test_components
+    assert_equal 'urn', @uri.scheme
+    assert_equal 'nid', @uri.nid
+    assert_equal 'nss', @uri.nss
+  end
+
+  def test_equal
+    assert_equal URI.parse('urn:nid:nss'), URI.parse('URN:NID:nss')
+
+    assert (URI.parse('urn:nid:nss') == URI.parse('URN:NID:nss'))
   end
 end
 
