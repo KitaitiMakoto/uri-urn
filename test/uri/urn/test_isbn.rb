b121431c594f04381836bd7ea1e1621e7eb16afa
Matthew Johnston <warmwaffles@gmail.com>
2014-07-29 17:34:56 -0500

    Make URI::URN utilize minitest

--- test/uri/urn/test_isbn.rb
+++ test/uri/urn/test_isbn.rb
@@ -1,41 +1,46 @@
-require 'test/unit'
+require 'minitest/autorun'
 require 'uri/urn/isbn'
 
-module URI::URN
+module URI
+  module URN
+    class TestISBN < MiniTest::Unit::TestCase
+      def setup
+        @isbn_string = '978-1937785499'
+      end
 
-class TestISBN < Test::Unit::TestCase
-  def setup
-    @isbn_string = '978-1937785499'
-  end
+      def test_parse
+        uri = URI.parse("urn:isbn:#{@isbn_string}")
+        assert_instance_of(ISBN, uri)
+        assert_equal('urn', uri.scheme)
+        assert_equal('isbn', uri.nid)
+        assert_equal(@isbn_string, uri.nss)
 
-  def test_parse
-    uri = URI.parse("urn:isbn:#{@isbn_string}")
+        assert_raises(URI::InvalidComponentError) do
+          URI.parse("urn:isbn:#{@isbn_string.chop}")
+        end
+      end
 
-    assert_instance_of ISBN, uri
-    assert_equal 'urn', uri.scheme
-    assert_equal 'isbn', uri.nid
-    assert_equal @isbn_string, uri.nss
-    assert_raise URI::InvalidComponentError do
-      URI.parse("urn:isbn:#{@isbn_string.chop}")
+      def test_build
+        uri = URI::URN::ISBN.build([@isbn_string])
+
+        assert_instance_of ISBN, uri
+        assert_equal('urn', uri.scheme)
+        assert_equal('isbn', uri.nid)
+        assert_equal(@isbn_string, uri.nss)
+        assert_raises(URI::InvalidComponentError) do
+          URI::URN::ISBN.build([@isbn_string.chop])
+        end
+      end
+
+      def test_equal
+        a = URI.parse('urn:isbn:193435693X')
+        b = URI.parse('urn:isbn:193435693X')
+        assert_equal(a, b)
+
+        a = URI.parse('urn:isbn:978-4274064616')
+        b = URI.parse('urn:isbn:9784274064616')
+        assert_equal(a, b)
+      end
     end
   end
-
-  def test_build
-    uri = URI::URN::ISBN.build([@isbn_string])
-
-    assert_instance_of ISBN, uri
-    assert_equal 'urn', uri.scheme
-    assert_equal 'isbn', uri.nid
-    assert_equal @isbn_string, uri.nss
-    assert_raise URI::InvalidComponentError do
-      URI::URN::ISBN.build([@isbn_string.chop])
-    end
-  end
-
-  def test_equal
-    assert URI.parse('urn:isbn:193435693X') == URI.parse('urn:isbn:193435693X')
-    assert URI.parse('urn:isbn:978-4274064616') == URI.parse('urn:isbn:9784274064616')
-  end
-end
-
 end
